c4adeccf707641bf61803e4d7860ed955520390b
CaoSuyang <caosuyang1026@gmail.com>
2021-08-02 11:22:25 +0800

    1.最大间距 2.有序数组的平方

--- leetcode/src/排序_数组/_977_有序数组的平方.java
+++ leetcode/src/排序_数组/_977_有序数组的平方.java
@@ -1,5 +1,17 @@
 package 排序_数组;
 
+/**
+ * https://leetcode-cn.com/problems/squares-of-a-sorted-array/
+ * @author myfolders
+ *
+ */
 public class _977_有序数组的平方 {
+	/**
+	 * 请你设计时间复杂度为 O(n) 的算法解决本问题
+	 * 时间复杂度O(n)
+	 * 双指针
+	 */
+    public int[] sortedSquares(int[] nums) {
 
+    }
 }
