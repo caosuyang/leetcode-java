de25cc9b1b136e9d3236fb5998a4e181bc909fe0
CaoSuyang <caosuyang1026@gmail.com>
2022-03-04 17:35:05 +0800

    面试题 01.04. 回文排列

--- leetcode/src/String/_面试题_01_04_回文排列.java
+++ leetcode/src/String/_面试题_01_04_回文排列.java
@@ -1,10 +1,57 @@
 package String;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.HashMap;
+
+/**
+ * https://leetcode-cn.com/problems/palindrome-permutation-lcci/
+ * @author csy
+ *
+ */
 public class _面试题_01_04_回文排列 {
 
+	/**
+	 * A.哈希表
+	 * @param s
+	 * @return
+	 */
+    public static boolean canPermutePalindrome(String s) {
+    	s = s.toLowerCase();
+    	HashMap<Character, Integer> map = new HashMap<>();
+    	for (int i = 0; i < s.length(); i++) {
+			Character c = s.charAt(i);
+			if (map.containsKey(c)) {
+				Integer v = map.get(c);
+				v += 1;
+				map.put(c, v);
+			} else {
+				map.put(c, 1);
+			}
+		}
+    	// { =5, a=2, r=2, s=2, t=2, e=2, v=2, i=2, l=2, n=2, o=2}
+    	System.out.println(map);
+    	
+    	Object[] values = map.values().toArray();    	
+    	int count = 0;
+    	for (int i = 0; i < values.length; i++) {
+			Integer vInteger = (Integer) values[i];
+			if (vInteger % 2 != 0) {
+				count++;
+			}
+		}
+    	if (count > 1) {
+			return false;
+		}
+    	return true;
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+//		String pali = "Rats live on no evil star";
+		String pali = "AaBb//a";
+		System.out.println(canPermutePalindrome(pali));
 	}
 
 }
