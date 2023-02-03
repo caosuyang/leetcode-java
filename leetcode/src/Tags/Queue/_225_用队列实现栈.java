497d5a21ce7e0beb278af16e78dba479e4240875
CaoSuyang <caosuyang1026@gmail.com>
2023-02-03 09:11:09 +0800

    queue

---
+++ leetcode/src/Tags/Queue/_225_用队列实现栈.java
@@ -0,0 +1,36 @@
+package Tags.Queue;
+/**
+ * https://leetcode.cn/problems/implement-queue-using-stacks/description/
+ * @author csy
+ *
+ */
+public class _225_用队列实现栈 {
+    public MyStack() {
+
+    }
+    
+    public void push(int x) {
+
+    }
+    
+    public int pop() {
+
+    }
+    
+    public int top() {
+
+    }
+    
+    public boolean empty() {
+
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
--- leetcode/src/Tags/Stack/_232_用栈实现队列.java
+++ leetcode/src/Tags/Stack/_232_用栈实现队列.java
@@ -1,5 +1,73 @@
 package Tags.Stack;
 
-public class _232_用栈实现队列 {
+import java.util.Stack;
 
+/**
+ * https://leetcode.cn/problems/implement-queue-using-stacks/description/
+ * @author csy
+ *
+ */
+public class _232_用栈实现队列 {
+	/**
+	 * 栈
+	 */
+	private Stack<Integer> inStack;
+	private Stack<Integer> outStack;
+	/**
+	 * 实例化
+	 */
+    public _232_用栈实现队列() {
+    	inStack = new Stack<>();
+    	outStack = new Stack<>();
+    }
+    
+    /**
+     * 将元素 x 推到队列的末尾
+     * @param x
+     */
+    public void push(int x) {
+    	inStack.push(x);
+    }
+    
+    /**
+     * 从队列的开头移除并返回元素
+     * @return
+     */
+    public int pop() {
+    	checkOutStack();
+    	return outStack.pop();
+    }
+    
+    /**
+     * 返回队列开头的元素
+     * @return
+     */
+    public int peek() {
+    	checkOutStack();
+    	return outStack.peek();
+    }
+    
+    /**
+     * 如果队列为空，返回 true ；否则，返回 false
+     * @return
+     */
+    public boolean empty() {
+    	return inStack.isEmpty() && outStack.isEmpty();
+    }
+    
+    private void checkOutStack() {
+    	if (outStack.isEmpty()) {
+    		while (!inStack.isEmpty()) {
+    			outStack.push(inStack.pop());
+    		}
+		}
+	}
 }
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * let obj = MyQueue()
+ * obj.push(x)
+ * let ret_2: Int = obj.pop()
+ * let ret_3: Int = obj.peek()
+ * let ret_4: Bool = obj.empty()
+ */
