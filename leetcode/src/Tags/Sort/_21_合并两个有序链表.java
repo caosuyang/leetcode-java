8436c8ae3c768a2936478b04cdeec568c5eccdd7
CaoSuyang <caosuyang1026@gmail.com>
2023-02-07 18:09:21 +0800

    1.二叉树 2.排序

--- leetcode/src/Tags/Sort/_21_合并两个有序链表.java
+++ leetcode/src/Tags/Sort/_21_合并两个有序链表.java
@@ -19,12 +19,35 @@ import common.ListNode;
  */
 public class _21_合并两个有序链表 {
 	/**
-	 * 排序
+	 * 排序，虚拟头节点
 	 * @param list1
 	 * @param list2
 	 * @return
 	 */
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-    	return null;
+    	if (list1 == null) {
+			return list2;
+		}
+    	if (list2 == null) {
+			return list1;
+		}
+    	ListNode head = new ListNode(0);
+    	ListNode cur = head;
+    	while (list1 != null && list2 != null) {
+			if (list1.val <= list2.val) {
+				cur = cur.next = list1;
+				list1 = list1.next;
+			} else {
+				cur = cur.next = list2;
+				list2 = list2.next;
+			}
+		}
+    	if (list1 == null) {
+			cur.next = list2;
+		}
+    	if (list2 == null) {
+			cur.next = list1;
+		}
+    	return head.next;
     }
 }
