14f4c0d71803116ef81e294a8c2508ff729b7b74
caosuyang <caosuyang1026@gmail.com>
2023-02-07 21:18:30 +0800

    排序

--- leetcode/src/Tags/Sort/_23_合并K个升序链表.java
+++ leetcode/src/Tags/Sort/_23_合并K个升序链表.java
@@ -1,5 +1,9 @@
 package Tags.Sort;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import common.ListNode;
 
 /**
@@ -18,7 +22,34 @@ import common.ListNode;
  * }
  */
 public class _23_合并K个升序链表 {
+	/**
+	 * 数组，排序
+	 * 时间复杂度：O(nlogn)
+	 * 空间复杂度：O(n)
+	 * @param lists
+	 * @return
+	 */
     public ListNode mergeKLists(ListNode[] lists) {
-    	return null;
+    	if (lists == null || lists.length == 0) {
+			return null;
+		}
+    	List<ListNode> nodes = new ArrayList<>();
+    	for (ListNode node : lists) {
+    		while (node != null) {
+				nodes.add(node);
+				node = node.next;
+			}
+		}
+    	Comparator<ListNode> comparator = (ListNode node1, ListNode node2) -> {
+    		return node1.val - node2.val;
+    	};
+
+    	nodes.sort(comparator);
+    	ListNode head = new ListNode(0);
+    	ListNode cur = head;
+    	for (ListNode node : nodes) {
+			cur = cur.next = node;
+		}
+    	return head.next;
     }
 }
