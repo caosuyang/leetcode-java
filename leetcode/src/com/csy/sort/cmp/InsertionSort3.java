199420dae349bde85a52774c465364dc3115f7c1
CaoSuyang <caosuyang1026@gmail.com>
2023-02-09 09:34:13 +0800

    1.插入排序二分搜索再次优化

--- leetcode/src/com/csy/sort/cmp/InsertionSort3.java
+++ leetcode/src/com/csy/sort/cmp/InsertionSort3.java
@@ -6,8 +6,37 @@ package com.csy.sort.cmp;
  *
  */
 public class InsertionSort3 {
+//	/**
+//	 * 二分搜索优化
+//	 * 时间复杂度：O(n^2)
+//	 * 空间复杂度：O(1)
+//	 * 稳定性：稳定
+//	 * 属于原地算法
+//	 * @param array
+//	 * @return
+//	 */
+//	public int[] insertionSort(int[] array) {
+//		for (int begin = 1; begin < array.length; begin++) {
+//			// 初始cur指针指向尾部待排序的第一个位置
+//			int cur = begin;
+//			// 先将待插入元素备份
+//			int ele = array[cur];
+//			// 待插入位置是insertIndex，也就是[insertIndex, begin) 范围年内元素都比待插入元素大
+//			int insertIndex = insertIndex(array, cur);
+//			// 头部有序数据中比待插入元素大的，都朝着尾部方向挪动1个位置
+//			// 也就是将 [insertIndex, begin) 范围内的元素往右边挪动一个单位
+//			for (int i = cur; i > insertIndex; i--) {
+//				array[i] = array[i - 1];
+//			}
+//			// 将待插入元素放到最终合适的位置
+//			array[insertIndex] = ele;
+//		}
+//		return array;
+//	}
+	
 	/**
 	 * 二分搜索优化
+	 * 二次优化：把元素插入操作放到insert函数内部
 	 * 时间复杂度：O(n^2)
 	 * 空间复杂度：O(1)
 	 * 稳定性：稳定
@@ -19,24 +48,36 @@ public class InsertionSort3 {
 		for (int begin = 1; begin < array.length; begin++) {
 			// 初始cur指针指向尾部待排序的第一个位置
 			int cur = begin;
-			// 先将待插入元素备份
-			int ele = array[cur];
 			// 待插入位置是insertIndex，也就是[insertIndex, begin) 范围年内元素都比待插入元素大
 			int insertIndex = insertIndex(array, cur);
-			// 头部有序数据中比待插入元素大的，都朝着尾部方向挪动1个位置
-			// 也就是将 [insertIndex, begin) 范围内的元素往右边挪动一个单位
-			for (int i = begin; i > insertIndex; i--) {
-				array[i] = array[i - 1];
-			}
-			// 将待插入元素放到最终合适的位置
-			array[insertIndex] = ele;
+			insert(array, begin, insertIndex);
 		}
 		return array;
 	}
 	
 	/**
-	 * 查找v在有序数组array中待插入的位置，优化条件合并
-	 * 要求二分搜索返回的插入位置：第一个大于v的元素位置
+	 * 将cur位置元素插入到insertIndex位置
+	 * @param array
+	 * @param cur
+	 * @param insertIndex
+	 */
+	public void insert(int[] array, int begin, int insertIndex) {
+		// 初始cur指针指向尾部待排序的第一个位置
+		int cur = begin;
+		// 先将待插入元素备份
+		int ele = array[cur];
+		// 头部有序数据中比待插入元素大的，都朝着尾部方向挪动1个位置
+		// 也就是将 [insertIndex, begin) 范围内的元素往右边挪动一个单位
+		for (int i = cur; i > insertIndex; i--) {
+			array[i] = array[i - 1];
+		}
+		// 将待插入元素放到最终合适的位置
+		array[insertIndex] = ele;
+	}
+	
+	/**
+	 * 利用二分搜索找到 index 位置元素的待插入位置
+	 * 已经排好序数组的区间范围是 [0, index)
 	 * @param array
 	 * @param 
 	 * @return
@@ -45,6 +86,7 @@ public class InsertionSort3 {
 		int begin = 0, end = index;
 		while (begin < end) {
 			int mid = (begin + end) >> 1;
+		    //  array[index]<array[mid]
 			if (cmp(array, index, mid) < 0) {
 				end = mid;
 			} else {// v >= array[mid]
@@ -57,30 +99,30 @@ public class InsertionSort3 {
 	}
 	
 	
-	/**
-	 * 查找v在有序数组array中待插入的位置，优化条件合并
-	 * 要求二分搜索返回的插入位置：第一个大于v的元素位置
-	 * @param array
-	 * @param 
-	 * @return
-	 */
-	public int insertIndex1(int[] array, int v) {
-		if (array == null || array.length == 0) {
-			return -1;
-		}
-		int begin = 0, end = array.length;
-		while (begin < end) {
-			int mid = (begin + end) >> 1;
-			if (v < array[mid]) {
-				end = mid;
-			} else {// v >= array[mid]
-				// 插入位置是第一个大于v的元素位置
-				begin = mid + 1;
-			}
-		}
-		// v >= array[mid]时，插入位置正好为begin
-		return begin;
-	}
+//	/**
+//	 * 查找v在有序数组array中待插入的位置，优化条件合并
+//	 * 要求二分搜索返回的插入位置：第一个大于v的元素位置
+//	 * @param array
+//	 * @param 
+//	 * @return
+//	 */
+//	public int insertIndex1(int[] array, int v) {
+//		if (array == null || array.length == 0) {
+//			return -1;
+//		}
+//		int begin = 0, end = array.length;
+//		while (begin < end) {
+//			int mid = (begin + end) >> 1;
+//			if (v < array[mid]) {
+//				end = mid;
+//			} else {// v >= array[mid]
+//				// 插入位置是第一个大于v的元素位置
+//				begin = mid + 1;
+//			}
+//		}
+//		// v >= array[mid]时，插入位置正好为begin
+//		return begin;
+//	}
 	
 	/**
 	 * 比较数组中e1和e2两个元素值的大小
