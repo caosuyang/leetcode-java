163d89cc5ade3f514c21a086291c219eaa47248e
caosuyang <caosuyang1026@gmail.com>
2023-02-19 20:57:25 +0800

    贪心-最优装载问题-加勒比海盗

---
+++ leetcode/src/Greedy/练习2_零钱兑换.java
@@ -0,0 +1,31 @@
+package Greedy;
+
+/**
+ 假设有 25 分、10 分、5 分、1 分的硬币，现要找给客户 41 分的零钱，如何办到硬币个数最少？
+◼ 贪心策略：每一次都优先选择面值最大的硬币
+① 选择 25 分的硬币，剩 16 分 ② 选择 10 分的硬币，剩 6 分 ③ 选择 5 分的硬币，剩 1 分 ④ 选择 1 分的硬币
+最终的解是共 4 枚硬币
+✓ 25 分、10 分、5 分、1 分硬币各一枚
+ * @author myfolders
+ *
+ */
+public class 练习2_零钱兑换 {
+	/**
+	 * 
+	 * @param capacity
+	 * @param weights
+	 * @return
+	 */
+	public int maxCount(int capacity, int[] weights) {
+		
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int capacity = 30;
+		int[] weights = {3, 5, 4, 10, 7, 14, 2, 11};
+		
+	}
+
+}
