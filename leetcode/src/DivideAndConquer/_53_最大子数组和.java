598dc52e5538e2e405935460775baa713e894f33
CaoSuyang <caosuyang1026@gmail.com>
2023-02-21 17:59:52 +0800

    分治-最大连续子序列和

--- leetcode/src/DivideAndConquer/_53_最大子数组和.java
+++ leetcode/src/DivideAndConquer/_53_最大子数组和.java
@@ -1,6 +1,7 @@
 package DivideAndConquer;
 
 /**
+ * 练习1 – 最大连续子序列和
  * https://leetcode.cn/problems/maximum-subarray/
  * @author csy
  *
@@ -8,11 +9,38 @@ package DivideAndConquer;
 public class _53_最大子数组和 {
 
 	/**
-	 * 
+	 * 方法一 暴力
 	 * @param nums
 	 * @return
 	 */
     public int maxSubArray(int[] nums) {
+    	return 0;
+    }
+    
+	/**
+	 * 方法二 暴力优化
+	 * @param nums
+	 * @return
+	 */
+    public int maxSubArray1(int[] nums) {
+    	return 0;
+    }
+    
+	/**
+	 * 方法三 分治
+	 * @param nums
+	 * @return
+	 */
+    public int maxSubArray2(int[] nums) {
+    	return 0;
+    }
+    
+	/**
+	 * 方法四 大数乘法
+	 * @param nums
+	 * @return
+	 */
+    public int maxSubArray3(int[] nums) {
     	return 0;
     }
 	
