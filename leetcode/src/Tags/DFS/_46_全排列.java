abcee4be83f8ab372b09ea92d1eb171e66128654
CaoSuyang <caosuyang1026@gmail.com>
2023-03-10 13:30:07 +0800

    1.追溯-全排列 2.追溯-括号生成

--- leetcode/src/Tags/DFS/_46_全排列.java
+++ leetcode/src/Tags/DFS/_46_全排列.java
@@ -4,10 +4,27 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
+ * 回溯
  * https://leetcode-cn.com/problems/permutations/
  * @author csy
  *
  */
+/**
+给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+示例 1：
+输入：nums = [1,2,3]
+输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+示例 2：
+输入：nums = [0,1]
+输出：[[0,1],[1,0]]
+示例 3：
+输入：nums = [1]
+输出：[[1]]
+提示：
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+nums 中的所有整数 互不相同
+*/
 public class _46_全排列 {
 	private List<List<Integer>> list;
 	private int[] nums;
--- leetcode/src/Tags/DFS/_46_全排列3.java
+++ leetcode/src/Tags/DFS/_46_全排列3.java
@@ -7,6 +7,22 @@ import java.util.List;
  * @author csy
  *
  */
+/**
+给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+示例 1：
+输入：nums = [1,2,3]
+输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+示例 2：
+输入：nums = [0,1]
+输出：[[0,1],[1,0]]
+示例 3：
+输入：nums = [1]
+输出：[[1]]
+提示：
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+nums 中的所有整数 互不相同
+*/
 public class _46_全排列3 {
     public List<List<Integer>> permute(int[] nums) {
         if (nums == null) return null;
